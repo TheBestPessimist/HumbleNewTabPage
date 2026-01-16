5f8aa8253d5d73a434ac13c3923f245167cd6c3a
TheBestPessimist <cristian@tbp.land>
2026-01-16 12:54:14 +0200

    perf: use template literals in saveColumns (# 108)

--- newtab.js
+++ newtab.js
@@ -1364,15 +1364,14 @@ async function loadColumns() {
 
 // saves current column configuration to storage
 function saveColumns() {
-    // clear previous config - forEachColumnEntry already uses string concat
-    forEachColumnEntry((x, y) => localStorage.removeItem('column.' + x + '.' + y));
+    // clear previous config
+    forEachColumnEntry((x, y) => localStorage.removeItem(`column.${x}.${y}`));
     verifyColumns();
-    // save new config - use string concat for better performance
+    // save new config
     for (let x = 0, xLen = columns.length; x < xLen; x++) {
         const col = columns[x];
-        const rowPrefix = 'column.' + x + '.';
         for (let y = 0, yLen = col.length; y < yLen; y++) {
-            localStorage.setItem(rowPrefix + y, col[y]);
+            localStorage.setItem(`column.${x}.${y}`, col[y]);
         }
     }
     loadColumns();
