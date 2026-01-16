8f29ce72ae9b78487daa96870424c643599233f5
TheBestPessimist <cristian@tbp.land>
2026-01-16 12:35:00 +0200

    perf: use string concat for menu position CSS values (# 96)

--- newtab.js
+++ newtab.js
@@ -813,8 +813,8 @@ function renderMenu(items, x, y) {
     }
 
     document.body.append(ul);
-    ul.style.left = `${Math.max(Math.min(x, window.innerWidth + window.scrollX - ul.clientWidth), 0)}px`;
-    ul.style.top = `${Math.max(Math.min(y, window.innerHeight + window.scrollY - ul.clientHeight), 0)}px`;
+    ul.style.left = Math.max(Math.min(x, window.innerWidth + window.scrollX - ul.clientWidth), 0) + 'px';
+    ul.style.top = Math.max(Math.min(y, window.innerHeight + window.scrollY - ul.clientHeight), 0) + 'px';
     ul.onmousedown = e => {
         e.stopPropagation();
         return true;
